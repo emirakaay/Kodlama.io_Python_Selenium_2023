1c8cfbf37206472465c0b9af614d6318003a0a6e
Emir Akay <88863902+emirakaay@users.noreply.github.com>
2023-03-22 19:08:59 +0300

    ders3odev1

---
+++ ders3odev1.py
@@ -0,0 +1,27 @@
+#HTML NEDİR?
+
+# Web tasarım konusunda araştırma yapan hemen herkesin karşına çıkan temel kavramlardan biri de HTML'dir.
+# Web sayfalarını oluşturma aşamasında kullanılan standart bir metin işaret dili olan HTML'nin açılımı “Hyper Text Markup Language” olarak bilinir.
+# Genel bilinen yanlış kanının aksine HTML bir programlama dili değildir. 
+# Daha açık anlatmak gerekirse, Chrome, Firefox, Yandex gibi tarayıcıların okuyup anlamlandırdığı dil HTML dilidir.
+
+
+#HTML LOCATORS NEDİR?
+
+# Selenium IDE'ye hangi web tabanlı objeler üzerinde çalışması gerektiğini söyleyen bir komuttur.
+# Doğru elementin tanımlanması, otomasyon oluşturmanın ön koşuludur.
+# "By" objesi ile  ile "ID, Name, ClassName, TagName, LinkText, CssSelector, xPath" lere erişilebilir.
+
+
+#SELENIUM AKSİYONLARI:
+
+# click = Seçilen öğeye tıklamak için kullanılır.
+# clear = Seçilen öğenin içeriğini temizlemek için kullanılır.
+# submit = Bir formu göndermek için kullanılır.
+# select_by_visible_text = Bir dropdown öğesinde belirli bir metne tıklamak için kullanılır.
+# Scroll = Sayfayı aşağı veya yukarı kaydırmak için kullanılır.
+# Switch to Frame = Bir iframe içine geçmek için kullanılır.
+# Switch to Window = Bir pencereye geçmek için kullanılır.
+# Back = Önceki sayfaya gitmek için kullanılır.
+# Forward = Sonraki sayfaya gitmek için kullanılır.
+# Refresh = Sayfayı yenilemek için kullanılır.
